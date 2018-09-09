17e6dc0bf9ee31006133bc4976276b480ff4709e
Ted Yeh <yeh.ted@gmail.com>
2018-09-08 19:57:58 -0500

    first commit

---
+++ src/vendingmachine.kt
@@ -0,0 +1,87 @@
+import javax.print.DocFlavor
+
+/**
+ * You MUST implement this interface.
+ */
+interface VendingMachine {
+    /**
+     * User Function - This is called when a user presses a button for a particular product. This is used for both price
+     * checking and purchasing.
+     */
+    fun buttonPress(productPosition: Int?)
+
+    /**
+     * User Function - This is called when the user adds money to the machine. The cents parameter represent the value
+     * of the particular currency added to the machine. For example, when the user adds a Nickel, this function will be
+     * called with a value of 5.
+     *
+     *
+     * Note: Only one coin will be added at a time. Only Nickels, Dimes, and Quarters will be added.
+     */
+    fun addUserMoney(cents: Int?)
+}
+
+/**
+ * This interface is intended to abstract the interaction with the vending machine's user interface.
+ * <p>
+ * Note: These functions do not validate if they function correctly, meaning you can call `dispenseProduct` even if the
+ * machine does not contain any products, however you should take care to NOT make a call the cannot be completed.
+ * Example: do not call `dispenseProduct` for a product that is out of stock.
+ */
+interface VendingMachineHardwareFunctions {
+    fun showMessage(message: String) {
+        println(message)
+    }
+
+    fun dispenseProduct(productPosition: Int?, productName: String?) {
+        val nullSafeProductName = productName ?: "ProductNum"+productPosition!!
+        println("Dispensing $nullSafeProductName from position $productPosition")
+    }
+
+    fun dispenseChange(changeInCents: Int?) {
+        println("Dispensing $changeInCents cents")
+    }
+}
+
+interface VendingMachineAdminFunctions {
+    fun addProduct(product: Product?) {
+        TODO()
+    }
+}
+
+class VendingMachineHardwareFunctionsImpl : VendingMachineHardwareFunctions
+class VendingMachineAdminFunctionsImpl: VendingMachineAdminFunctions
+
+data class Product(val name: String, var price: Int)
+
+class VendingMachineImpl(var products: Map<Int, Product>) : VendingMachine {
+    var hardwareFunctions = VendingMachineHardwareFunctionsImpl()
+    var adminFunctionsImpl = VendingMachineAdminFunctionsImpl()
+
+    var userMoney: Int = 0
+
+    override fun buttonPress(productPosition: Int?) {
+        val price = products[productPosition]?.price
+        val name = products[productPosition]?.name
+        hardwareFunctions.showMessage("Name is $name, amount is $price")
+
+        if (userMoney >= price!!) {
+            hardwareFunctions.dispenseProduct(productPosition, name)
+            hardwareFunctions.dispenseChange(userMoney - price)
+            userMoney = 0
+        }
+    }
+
+    override fun addUserMoney(cents: Int?) {
+        userMoney += cents!!
+    }
+}
+
+fun main(args: Array<String>) {
+    val obj = VendingMachineImpl(hashMapOf(0 to Product("foo", 10), 1 to Product("bar", 45)))
+
+    println("Calling addUserMoney(25): ${obj.addUserMoney(25)}")
+    println("Calling addUserMoney(25): ${obj.addUserMoney(25)}")
+    println("Calling buttonPress(1): ${obj.buttonPress(1)}")
+    println("Calling buttonPress(0): ${obj.buttonPress(0)}")
+}
